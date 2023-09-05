de08ef9345c9aba23f3043988348e4b98fa31e2b
Tim Spiridonov <timour.spiridonov0gmail.com>
2023-09-05 10:22:57 +0100

    version with hichart built

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const options = {
+    chart: {
+      type: "bar", // You can change the chart type (e.g., 'line', 'pie', etc.)
+    },
+    title: {
+      text: "Basic Highcharts Example", // Chart title
+    },
+    xAxis: {
+      categories: ["Category 1", "Category 2", "Category 3"], // X-axis categories
+    },
+    yAxis: {
+      title: {
+        text: "Value", // Y-axis label
+      },
+    },
+    series: [
+      {
+        name: "Series 1", // Series name
+        data: [10, 20, 30], // Data points for the series
+      },
+    ],
+  };
   return (
     <>
-   
+      <HighchartsReact highcharts={Highcharts} options={options} />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -21,7 +43,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
